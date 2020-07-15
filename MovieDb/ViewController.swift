3d813d8b8224d2fcd021e7c1ef5b0d21163ca370
Camilo Vargas <camilovargas@Luchos-MacBook-3.local>
2020-07-14 19:06:49 -0500

    initial commit

---
+++ MovieDb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDb
+//
+//  Created by Camilo Vargas on 14/07/20.
+//  Copyright © 2020 Camilo Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
